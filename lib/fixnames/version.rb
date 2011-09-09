855f6e7195aa863286a56cf3594186a1de12fbd4
Brent Sanders <git@thoughtnoise.net>
2011-09-09 15:59:05 -0700

    version bump

--- lib/fixnames/version.rb
+++ lib/fixnames/version.rb
@@ -2,8 +2,8 @@ class Fixnames
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
-    BUILD = 'pre1'
+    PATCH = 1
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
