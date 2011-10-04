3b00db22248ea628acd82bdbe9748ba6620cdffe
Brent Sanders <git@thoughtnoise.net>
2011-10-03 21:46:11 -0700

    version push

--- lib/fixnames/version.rb
+++ lib/fixnames/version.rb
@@ -1,8 +1,8 @@
 class Fixnames
   module Version
     MAJOR = 0
-    MINOR = 2
-    PATCH = 2
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
